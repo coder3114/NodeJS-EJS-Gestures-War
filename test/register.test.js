0e51d7d68a1b165f0f3bab02c6812a58a9550f83
coderella <qiangie.yang@gmail.com>
2023-08-05 16:38:07 +0100

    Implement registration functionality with index.js and register.ejs view

--- test/register.test.js
+++ test/register.test.js
@@ -7,7 +7,7 @@ const TESTPATH = `/`;
 
 chai.use(chaiHttp);
 
-describe("Register/Index route / GET", () => {
+describe("Register route / GET", () => {
   it(`should render the index view with a 200 status code and sends back the rendered HTML string`, async () => {
     await chai
       .request(app)
