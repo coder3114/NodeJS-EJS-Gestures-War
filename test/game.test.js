3a39d0f18ab4f8271828322b45d330946d3541d0
coderella <qiangie.yang@gmail.com>
2023-08-05 20:07:17 +0100

    Add game route tests

---
+++ test/game.test.js
@@ -0,0 +1,50 @@
+const chai = require("chai"),
+  expect = chai.expect;
+const chaiHttp = require("chai-http");
+const app = require("../app.js");
+
+const TESTPATH = "/game";
+
+chai.use(chaiHttp);
+
+let res;
+
+describe("Game route", () => {
+  beforeEach(() => {
+    res = null;
+  });
+
+  describe("POST /", () => {
+    it("should redirect to /game", async () => {
+      const testData = {
+        player: "John Smith",
+      };
+      res = await chai
+        .request(app)
+        .post(TESTPATH)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send(testData);
+      expect(res).to.redirect;
+    });
+  });
+
+  describe("GET /", () => {
+    it("should have a 200 status code and sends back the rendered HTML string", async () => {
+      res = await chai
+        .request(app)
+        .get(TESTPATH)
+        .set("content-type", "application/x-www-form-urlencoded");
+      expect(res).to.have.status(200);
+      expect(res).to.be.html;
+    });
+
+    it("should render the game view with player's name", async () => {
+      res = await chai
+        .request(app)
+        .get("/game")
+        .set("content-type", "application/x-www-form-urlencoded");
+      expect(res.text).to.contain("<h3>Hi John Smith!</h3>");
+      expect(res.text).to.contain("Please provide your choice:");
+    });
+  });
+});
