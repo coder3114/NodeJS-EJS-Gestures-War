d6768924e9f047deafa4966a95890c257e086977
coderella <qiangie.yang@gmail.com>
2023-08-06 20:01:06 +0100

    Apply basic styling and Bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const registerRouter = require("./routes/register.js");
 const gameRouter = require("./routes/game.js");
